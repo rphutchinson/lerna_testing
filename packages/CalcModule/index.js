e7c5877cdee45b455565f76f7efd4342e94a61e5
Patrick Hutchinson <rphutchinson@gmail.com>
2020-05-19 16:29:02 -0400

    initial setup

---
+++ packages/CalcModule/index.js
@@ -0,0 +1,5 @@
+const additionModule = require("AdditionModule");
+const subtractionModule = require("SubtractionModule");
+
+console.log(`Add 4 + 5 = ${additionModule.add(4, 5)}`);
+console.log(`Subtract 5 - 4 = ${subtractionModule.subtract(5, 4)}`);
