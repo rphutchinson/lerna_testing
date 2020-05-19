1437d5c4f23e6b88de01108b224461c1e09d6c1f
Patrick Hutchinson <rphutchinson@gmail.com>
2020-05-19 16:41:40 -0400

    added a comment

--- packages/CalcModule/index.js
+++ packages/CalcModule/index.js
@@ -1,5 +1,6 @@
 const additionModule = require("AdditionModule");
 const subtractionModule = require("SubtractionModule");
 
+// This is a test of our modules
 console.log(`Add 4 + 5 = ${additionModule.add(4, 5)}`);
 console.log(`Subtract 5 - 4 = ${subtractionModule.subtract(5, 4)}`);
